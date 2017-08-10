12a33addb96c87b2fa63c27dbd8a92b9d5ac0595
Russell Endicott <russell.endicott@ge.com>
2017-08-10 00:02:08 -0400

    Adding tests for current handlers

--- fhid/handlers_test.go
+++ fhid/handlers_test.go
@@ -1,19 +1,28 @@
 package fhid
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"github.build.ge.com/212601587/fhid/fhidLogger"
 )
 
+type ImagePostResponse struct {
+	Success string
+	Data    string
+}
+
 const imageGood = `
 {
-"Version": "1.2.3.145",
-"BaseOS": "Ubuntu14.04",
-"ReleaseNotes": "Did the thing"
+"Version":"1.2.3.145",
+"BaseOS":"Ubuntu14.04",
+"ReleaseNotes":"Did the thing"
 }
 `
 
@@ -29,10 +38,10 @@ func setup() error {
 }
 
 func initLog() {
-	fhidLogger.SetLogger(false, "fhid_test.log.json", "debug")
+	fhidLogger.SetLogger(false, "fhid_test.log.json", "info")
 }
 
-func TestImagePost(t *testing.T) {
+func TestImageGetNone(t *testing.T) {
 	err := setup()
 	initLog()
 	if err != nil {
@@ -46,22 +55,63 @@ func TestImagePost(t *testing.T) {
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(HandlerImages)
 	handler.ServeHTTP(rr, req)
-	/*
-		if status := rr.Code; status != http.StatusOK {
-			t.Errorf("handler returned wrong status code: got %v want %v",
-				status, http.StatusOK)
-		}
-		// Check the status code is what we expect.
-		if status := rr.Code; status != http.StatusOK {
-			t.Errorf("handler returned wrong status code: got %v want %v",
-				status, http.StatusOK)
-		}
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusBadRequest)
+	}
+	// Check the response body is what we expect.
+	expected := `{"Error": "Key 'ImageId' not found in URL string."}` + "\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
 
-		// Check the response body is what we expect.
-		expected := `{"alive": true}`
-		if rr.Body.String() != expected {
-			t.Errorf("handler returned unexpected body: got %v want %v",
-				rr.Body.String(), expected)
-		}
-	*/
+func TestImagePostGet(t *testing.T) {
+	err := setup()
+	initLog()
+	if err != nil {
+		t.Errorf("Unable to connect to Redis for testing: %s", err)
+	}
+
+	// First we need to post an entry to the DB
+	postBody := bytes.NewBufferString(imageGood)
+	req, err := http.NewRequest("POST", "/images", postBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HandlerImages)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	var j ImagePostResponse
+	err = json.Unmarshal([]byte(rr.Body.String()), &j)
+	if err != nil {
+		t.Errorf("Unable to unmarshal response JSON: %s", err)
+	}
+	fmt.Printf("Parsed j.Data into '%s'", j.Data)
+
+	// now we retrieve the entry
+	uriQuery := fmt.Sprintf("/images?ImageId=%s", j.Data)
+	req, err = http.NewRequest("GET", uriQuery, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr = httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := strings.Replace(imageGood, "\n", "", -1)
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
