93c3d84a3441616f19bca874d6cf32299032c879
Russell Endicott <russell.endicott@ge.com>
2017-08-11 15:08:47 -0400

    Trying this

--- pipeline_build/pre-reqs.sh
+++ pipeline_build/pre-reqs.sh
@@ -4,4 +4,3 @@ go get github.com/garyburd/redigo/redis
 go get github.com/inconshreveable/log15
 go get github.com/satori/go.uuid
 go get github.com/youtube/vitess/go/pools
-go get golang.org/x/net/context
