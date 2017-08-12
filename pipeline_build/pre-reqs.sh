ef3face0027c24bb59ee5dac96c1593056309d77
Russell Endicott <russell.endicott@ge.com>
2017-08-12 00:22:51 -0400

    switching to new base docker due to cert issue

--- pipeline_build/pre-reqs.sh
+++ pipeline_build/pre-reqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go get -insecure github.com/garyburd/redigo/redis
-go get -insecure github.com/inconshreveable/log15
-go get -insecure github.com/satori/go.uuid
-go get -insecure github.com/youtube/vitess/go/pools
+go get github.com/garyburd/redigo/redis
+go get github.com/inconshreveable/log15
+go get github.com/satori/go.uuid
+go get github.com/youtube/vitess/go/pools
