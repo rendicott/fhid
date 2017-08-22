bf7568225ad8a3f8e5bf9fff116d2eaf744a25b6
Russell Endicott <russell.endicott@ge.com>
2017-08-22 00:22:08 -0400

    Adding miniredis prereq

--- pipeline_build/pre-reqs.sh
+++ pipeline_build/pre-reqs.sh
@@ -5,3 +5,4 @@ go get github.com/inconshreveable/log15
 go get github.com/satori/go.uuid
 go get github.com/youtube/vitess/go/pools
 go get golang.org/x/net/context
+go get github.com/alicebob/miniredis
